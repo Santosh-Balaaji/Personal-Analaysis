704308f325036b287aec8df2d206d8c80191f357
Santosh Balaaji Selvakumar <sv.santosh2695@gmail.com>
2020-03-18 09:21:02 +1300

    refactored the code

--- src/Sections/Section1.js
+++ src/Sections/Section1.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import clsx from 'clsx';
-import Checkbox from '@material-ui/core/Checkbox';
 import {Grid,Box,Paper,Slider,Typography} from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Container from '@material-ui/core/Container';
-import { grey } from '@material-ui/core/colors';
 
 const useStyles = makeStyles(theme => ({
     root: {
       flexGrow: 1,
-      color:grey,
     },
     paper: {
         height: '8rem',
@@ -21,30 +12,6 @@ const useStyles = makeStyles(theme => ({
         padding: '10px 10px',
         margin: `${theme.spacing(1)}px auto`,
         boxShadow: '0 3px 5px 2px rgba(135, 135, 171, .3)',
-
-    
-    },
-    item:{
-        background: 'linear-gradient(45deg, #e1e1ea 30%, #FFFF 90%)',
-        border: 5,
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        height: '8rem',
-        width:'69rem',
-        padding: '10px 0px',
-        bgcolor:"grey.500",
-        color:"black",
-        p: 2,
-        position: "absolute",
-        top: 100,
-        left: 120,
-    },
-    container:{
-        bgcolor: 'background.paper',
-        m: 1,
-        style: { width: '5rem', height: '5rem' },
-        borderColor: 'text.primary',
-        border: 1
     },
     margin: {
         height: theme.spacing(3),

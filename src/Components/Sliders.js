b49859ac9e5b5729570e561d691222e30f4fbbe0
Santosh Balaaji Selvakumar <sv.santosh2695@gmail.com>
2020-03-18 11:18:08 +1300

    state added and section1 sliders added

---
+++ src/Components/Sliders.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {Grid,Paper,Slider,Typography} from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+        height: '8rem',
+        width:'69rem',
+        padding: '10px 10px',
+        margin: `${theme.spacing(1)}px auto`,
+        boxShadow: '0 3px 5px 2px rgba(135, 135, 171, .3)',
+    },
+    margin: {
+        height: theme.spacing(3),
+        left: 400
+      },
+      slider:{
+          width: 400,
+          left: 400
+      }
+
+  }));
+  const marks = [
+    {
+      value: 1,
+      label: 'Strongly Disagree',
+    },
+    {
+      value: 2,
+      label: 'Disagree',
+    },
+    {
+      value: 3,
+      label: 'Neutral',
+    },
+    {
+      value: 4,
+      label: 'Agree',
+    },
+    {
+      value: 5,
+      label: 'Strongly Agree',
+    },
+  ];
+  
+  function valuetext(value) {
+    return `${value}`;
+  }
+  function onChange(event,value){
+    console.log(value);
+  }
+
+export default function Sliders(props){
+    const classes = useStyles();
+    return(
+<Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item xs >
+          <div className={classes.margin} />
+      <Typography id="discrete-slider-custom" gutterBottom >
+        {props.sliderTypo}					
+      </Typography>
+      <div className={classes.slider} style={{marginLeft:'25em'}}>
+      <Slider
+        defaultValue={1}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider-custom"
+        step={1}
+        valueLabelDisplay="auto"
+        marks={marks}
+        min={1}
+        max={5}
+        onChange={(event,value)=>props.onChange(event,value)}
+        />
+        </div>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+}
--- src/Sections/Section1.js
+++ src/Sections/Section1.js
@@ -1,139 +1,175 @@
 import React from 'react';
-import {Grid,Box,Paper,Slider,Typography} from '@material-ui/core';
+import {Grid,Box,} from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
+import Sliders from '../Components/Sliders';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-        height: '8rem',
-        width:'69rem',
-        padding: '10px 10px',
-        margin: `${theme.spacing(1)}px auto`,
-        boxShadow: '0 3px 5px 2px rgba(135, 135, 171, .3)',
-    },
-    margin: {
-        height: theme.spacing(3),
-        left: 400
-      },
-      slider:{
-          width: 400,
-          left: 400
-      }
-
-  }));
   
-  export default function CenteredGrid() {
-    const classes = useStyles();
-    const [state, setState] = React.useState({
-        gilad: true,
-        jason: false,
-        antoine: false,
-      });
-    const handleChange = name => event => {
-        setState({ ...state, [name]: event.target.checked });
-      };
-    
-      const { gilad, jason, antoine } = state;
+  export default class Section1 extends React.Component {
+      state={
+          slider1:1,
+          slider2:1,
+          slider3:1,
+          slider4:1,
+          slider5:1,
+          slider6:1,
+          slider7:1,
+          slider8:1,
+          slider9:1,
+          slider10:1,
+          slider11:1,
+          slider12:1,
+          slider13:1,
+          slider14:1,
+          slider15:1,
+          slider16:1,
+          slider17:1,
+          slider18:1,
+          slider19:1,
+          slider20:1,
 
+      }
+      
+    onChange1=(event,value)=>{
+        this.setState({slider1: value},function(){
+            console.log(this.state.slider1);
+        });
+    }
+    onChange2=(event,value)=>{
+        this.setState({slider2: value},function(){
+            console.log(this.state.slider2);
+        });
+    }
+    onChange3=(event,value)=>{
+        this.setState({slider3: value},function(){
+            console.log(this.state.slider3);
+        });
+    }
+    onChange4=(event,value)=>{
+        this.setState({slider4: value},function(){
+            console.log(this.state.slider4);
+        });
+    }
+    onChange5=(event,value)=>{
+        this.setState({slider5: value},function(){
+            console.log(this.state.slider5);
+        });
+    }
+    onChange6=(event,value)=>{
+        this.setState({slider6: value},function(){
+            console.log(this.state.slider6);
+        });
+    }
+    onChange7=(event,value)=>{
+        this.setState({slider7: value},function(){
+            console.log(this.state.slider7);
+        });
+    }
+    onChange8=(event,value)=>{
+        this.setState({slider8: value},function(){
+            console.log(this.state.slider8);
+        });
+    }
+    onChange9=(event,value)=>{
+        this.setState({slider9: value},function(){
+            console.log(this.state.slider9);
+        });
+    }
+    onChange10=(event,value)=>{
+        this.setState({slider10: value},function(){
+            console.log(this.state.slider10);
+        });
+    }
+    onChange11=(event,value)=>{
+        this.setState({slider11: value},function(){
+            console.log(this.state.slider11);
+        });
+    }
+    onChange12=(event,value)=>{
+        this.setState({slider12: value},function(){
+            console.log(this.state.slider12);
+        });
+    }
+    onChange13=(event,value)=>{
+        this.setState({slider13: value},function(){
+            console.log(this.state.slider13);
+        });
+    }
+    onChange14=(event,value)=>{
+        this.setState({slider14: value},function(){
+            console.log(this.state.slider14);
+        });
+    }
+    onChange15=(event,value)=>{
+        this.setState({slider15: value},function(){
+            console.log(this.state.slider15);
+        });
+    }
+    onChange16=(event,value)=>{
+        this.setState({slider16: value},function(){
+            console.log(this.state.slider16);
+        });
+    }
+    onChange17=(event,value)=>{
+        this.setState({slider17: value},function(){
+            console.log(this.state.slider17);
+        });
+    }
+    onChange18=(event,value)=>{
+        this.setState({slider18: value},function(){
+            console.log(this.state.slider18);
+        });
+    }
+    onChange19=(event,value)=>{
+        this.setState({slider19: value},function(){
+            console.log(this.state.slider19);
+        });
+    }
+    onChange20=(event,value)=>{
+        this.setState({slider20: value},function(){
+            console.log(this.state.slider20);
+        });
+    }
+
+    
+      render(){
       const defaultProps = {
         bgcolor: 'background.paper',
         m: 6,   
         style: { width: '70rem', height: '60rem' },
         borderColor: 'grey.500',
-        
       };
-      const marks = [
-        
-        {
-          value: 1,
-          label: 'Strongly Disagree',
-        },
-        {
-          value: 2,
-          label: 'Disagree',
-        },
-        {
-          value: 3,
-          label: 'Neutral',
-        },
-        {
-          value: 4,
-          label: 'Agree',
-        },
-        {
-          value: 5,
-          label: 'Strongly Agree',
-        },
-      ];
       
-      function valuetext(value) {
-        return `${value}`;
-      }
-      
-  
     return (
       <div>
         <Box display="flex" justifyContent="center">
         <Box border={1} {...defaultProps}>
         <Grid container>
-            
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item xs>
-          <div className={classes.slider} style={{marginLeft:'25em'}}>
-          <div className={classes.margin} />
-      <Typography id="discrete-slider-custom" gutterBottom>
-        1. HAVE FEW ARTISTIC SKILLS		
-      </Typography>
-      <Slider
-        defaultValue={1}
-        getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider-custom"
-        step={1}
-        valueLabelDisplay="auto"
-        marks={marks}
-        min={1}
-        max={5}
-        />
-        </div>
-        </Grid>
-        </Grid>
-      </Paper>
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item xs >
-          <div className={classes.margin} />
-      <Typography id="discrete-slider-custom" gutterBottom >
-        2. INSISTS UNTIL COMPLETING THE WORK IN AN ORGANIZED MANNER					
-      </Typography>
-      <div className={classes.slider} style={{marginLeft:'25em'}}>
-      <Slider
-        defaultValue={1}
-        getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider-custom"
-        step={1}
-        valueLabelDisplay="auto"
-        marks={marks}
-        min={1}
-        max={5}
-        />
-        </div>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Paper className={classes.paper}>
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item xs>
-              Hello3
-          </Grid>
-        </Grid>
-      </Paper>
+      <Sliders sliderTypo={'1. HAVE FEW ARTISTIC SKILLS'} onChange={this.onChange1} />
+      <Sliders sliderTypo={'2. INSISTS UNTIL COMPLETING THE WORK IN AN ORGANIZED MANNER'} onChange={this.onChange2} />
+      <Sliders sliderTypo={'3. SOCIALLY OUTGOING'} onChange={this.onChange3} />
+      <Sliders sliderTypo={'4. USUALLY TRUSTS PEOPLE'} onChange={this.onChange4}/>
+      <Sliders sliderTypo={'5. RELAXED AND CONTROLS STRESS'}  onChange={this.onChange5}/>
+      <Sliders sliderTypo={'6. I ENJOY HEARING NEW IDEAS FROM OTHERS'}  onChange={this.onChange6}/>
+      <Sliders sliderTypo={'7. I MAKE PLANS AND STICK TO THEM'}  onChange={this.onChange7}/>
+      <Sliders sliderTypo={'8. I AM A LIFE OF PARTY'}  onChange={this.onChange8}/>
+      <Sliders sliderTypo={'9. I TAKE CARE OF OTHER PEOPLE BEFORE EMOTIONS TAKING CARE OF MYSELF'}  onChange={this.onChange9}/>
+      <Sliders sliderTypo={'10. I LOVE TO STAY IN PEACE'}  onChange={this.onChange10}/>
+      <Sliders sliderTypo={'11. HAVE A FERTILE IMAGINATION'}  onChange={this.onChange11}/>
+      <Sliders sliderTypo={'12. DOES THE WORK WITHOUT ANY PLANS'}  onChange={this.onChange12}/>
+      <Sliders sliderTypo={'13. RESERVED'}  onChange={this.onChange13}/>
+      <Sliders sliderTypo={'14. TENDS TO BE CRITICAL TOWARDS OTHERS'}  onChange={this.onChange14}/>
+      <Sliders sliderTypo={'15. GETS NERVOUS EASILY'}  onChange={this.onChange15}/>
+      <Sliders sliderTypo={'16. I AM NOT INTERESTED IN ABSTRACT IDEAS'}  onChange={this.onChange16}/>
+      <Sliders sliderTypo={'17. I DON’T GET MY CHORES DONE RIGHT AWAY'}  onChange={this.onChange17}/>
+      <Sliders sliderTypo={'18. I HAVE A LITTLE TO SAY'}  onChange={this.onChange18}/>
+      <Sliders sliderTypo={'19. I AM NOT REALLY INTERESTED IN OTHERS'}  onChange={this.onChange19}/>
+      <Sliders sliderTypo={'20. I AM OFTEN TROUBLED BY NEGATIVE THOUGHTS'}  onChange={this.onChange20}/>
+
         </Grid>
         </Box>
         </Box>
       </div>
     );
+      }
   }
   

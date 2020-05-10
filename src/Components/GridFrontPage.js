76c7d65dcb6b3483ab852ef18588b730adead984
Sureya Prakash P S <sunder.surya@gmail.com>
2020-05-10 19:35:59 +1200

    first page updated

---
+++ src/Components/GridFrontPage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import "../Sections/styles.scss";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    width:'90%',
+    height:'100%',
+    textAlign: 'center',
+    padding:'1%',
+    color: theme.palette.text.secondary,
+    marginLeft:'4%',
+    backgroundColor:'unset',
+    
+  },
+  
+}));
+
+export default function GridPage() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1} row style={{position:'absolute',top:'8em'}} >
+        <Grid item xs={12} lg={4}>
+          <Paper className={classes.paper} elevation={18}>
+              <div className="header-text-font">BIG FIVE FACTOR THEORY</div>
+              <div className="text-font" style={{paddingTop:'2%'}}>The big five personality traits are the best accepted and most commonly used model of personality in academic psychology. The results of this test are not psychological or psychiatric advice of any kind and come with no guarantee of accuracy or fitness for a particular purpose to explore your personality with the highly respected big five-factor model. You will see how you will stack up the five dimensions: OCEAN
+               </div>   
+              </Paper>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Paper className={classes.paper} elevation={18}>
+              <div className="header-text-font">PROSPECTS THEORY</div>
+              <div className="text-font" style={{paddingTop:'2%'}}>Prospects theory is a behavioral model that shows how people decide between alternatives that involve risk and uncertainty (e.g. percentage likelihood of gains or losses). It demonstrates that people think in terms of expected utility relative to a reference point (e.g. current wealth) rather than absolute outcomes.
+               </div>   
+              </Paper>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Paper className={classes.paper} elevation={18}>
+              <div className="header-text-font">BEHAVIORAL BIASES</div>
+              <div className="text-font" style={{paddingTop:'2%'}}>While one’s biases can serve us well in our lives, they can have the opposite effect with investing. Investing behavioral biases encompass both cognitive and emotional biases. Cognitive biases stem from statistical, information processing or memory errors and emotional biases stem from intuition or impulse. 
+               </div>   
+              </Paper>
+        </Grid>
+        </Grid>
+       </div>
+  );
+}
--- src/Sections/IndexPage.js
+++ src/Sections/IndexPage.js
@@ -3,11 +3,16 @@ import { $CombinedState } from 'redux';
 import background from '../Images/woman.jpg';
 import { blue } from '@material-ui/core/colors';
 import {Image} from 'react-bootstrap';
-import { makeStyles,Grid } from "@material-ui/core";  
+import { makeStyles,Grid,Paper,Typography } from "@material-ui/core";  
  import "./styles.scss"; 
  import AutoGrid from '../Components/GridComponent';
 import NextButton from '../Components/NextButton'; 
 import {withRouter} from 'react-router-dom';
+import GridPage from '../Components/GridFrontPage';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+import FaceIcon from '@material-ui/icons/Face';
+import AlarmOffIcon from '@material-ui/icons/AlarmOff';
 
 class Index extends React.Component{
    
@@ -31,10 +36,57 @@ class Index extends React.Component{
                     <Image src={background} className="image"/>
                     <div className="slider-pos">
                      <h1 className="slider-text" style={{top:'-3%'}}>Personality Analysis</h1>
+                     <h1 className="slider-text"> Study On Behavioral Finance</h1>
                      </div>
                     </div>
-                    <h1>Hello World</h1>
+                    <div className="body-first-text-header">
+                        Why Behavioral Finance?</div>
+                    <div className="body-first-text">
+                    We make thousands of decisions every day. Do I cross the road now, or wait for the oncoming truck to pass? Should I eat fries or a salad for lunch? How much should I tip the cab driver? We usually make these decisions with almost no thought, using what psychologists call “heuristics” – rules of thumb that enable us to navigate our lives. Without these mental shortcuts, we would be paralyzed by the multitude of daily choices. But in certain circumstances, these shortcuts lead to predictable errors – predictable, that is, if we know what to watch out for. Did you know, for example, that we are naturally biased towards selling investments that are doing well for us, but holding on to those that are doing poorly? Or that we often select sub-optimal insurance payment plans, and routinely purchase insurance that we don’t even need? And why do so many of us fail to enrol in our employer’s corporate retirement plans, even when the employer offers to match our contributions? Behavioral finance is the study of these and dozens of other financial decision-making errors that can be avoided if we are familiar with the biases that cause them.
+                    </div>
+                    
+                    <div className="front-section-grid" >
+                        <GridPage />
+                        </div>
+                        <div style={{backgroundColor:'#f3f4f5', color:'#000',height:'27em'}}>
+                        <div style={{fontSize:'2.5rem'}}>
+                        INSTRUCTIONS
+                            </div>
+                            <div >
+                                <Grid container >
+                                    <Grid item xs={12} lg={6} style={{marginTop:'3em'}}>
+                                        <EmojiEmotionsIcon  style={{fontSize:'3em'}}/>
+                                        <Typography style={{fontSize:'1.3rem'}}>
+                                        Please respond to all the statements sequentially based on your true experiences
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={12} lg={6} style={{marginTop:'3em'}}>
+                                        <DoneAllIcon style={{fontSize:'3em'}}/>
+                                        <Typography style={{fontSize:'1.3rem'}}>
+                                        Double-check before moving on to next
+                                        </Typography>
+                                    </Grid>
+                                   
+                                    <Grid item xs={12} lg={6} style={{marginTop:'4em'}}>
+                                        <AlarmOffIcon style={{fontSize:'3em'}}/>
+                                        <Typography style={{fontSize:'1.3rem'}}>
+                                        This test is not time-bound
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={12} lg={6} style={{marginTop:'4em'}}>
+                                        <FaceIcon style={{fontSize:'3em'}}/>
+                                        <Typography style={{fontSize:'1.3rem'}}>
+                                        Describe yourself as you generally are now, not as you wish to be in the future
+                                        </Typography>
+                                    </Grid>
+
+                                    </Grid>
+
+
+                                </div>
+                                </div>
                     <NextButton onclick={this.handleClick} />
+
                 
                 </div>
         );
